24386b1f563e0ffc3232f546e9df00c0d29d5a65
Ryan Kim <kim1885@purdue.edu>
2022-03-23 15:00:41 -0500

    Add NPC button and add styling to card and card-list.

--- script.js
+++ script.js
@@ -1,21 +1,37 @@
 const shieldsButton = document.querySelector('#shieldsButton');
 const weaponsButton = document.querySelector('#weaponsButton');
+const npcsButton = document.querySelector('#npcsButton');
+
+const cardList = document.querySelector('.card-list')
 
 let receivedData;
 
 // Retrieve shield data and store it in an array
 const getShields = () => {
+  cardList.innerHTML = "";
   getData('shields');
 }
 
 const getWeapons = () => {
+  cardList.innerHTML = "";
   getData('weapons');
 }
 
+const getNpcs = () => {
+  cardList.innerHTML = "";
+  getData('npcs');
+}
+
 const createImageList = (data) => {
+  let card = document.createElement('div');
+  card.classList.add('card');
   let img = document.createElement('img');
   img.src = data.image;
-  document.querySelector('.card-list').appendChild(img);
+  let title = document.createElement('p');
+  title.innerHTML = data.name;
+  card.appendChild(img);
+  card.appendChild(title);
+  document.querySelector('.card-list').appendChild(card);
 }
 
 const getData = (dataType) => {
@@ -31,6 +47,8 @@ const getData = (dataType) => {
 
 shieldsButton.addEventListener('click', getShields);
 weaponsButton.addEventListener('click', getWeapons);
+npcsButton.addEventListener('click', getNpcs);
+
 
 
 
