a58606be18d857b06039778b212a64902df946cf
Ryan Kim <kim1885@purdue.edu>
2022-03-23 14:32:24 -0500

    Refactor getData function.

--- script.js
+++ script.js
@@ -5,29 +5,28 @@ let receivedData;
 
 // Retrieve shield data and store it in an array
 const getShields = () => {
-  const URL = 'https://eldenring.fanapis.com/api/shields?limit=100';
-  receivedData = fetch(URL)
-    .then(res => res.json())
-    .then(data => receivedData = data.data)
-    .then(() => console.log(receivedData))
-    .then(() => receivedData.forEach(data => createImageList(data)))
-    .then(err => console.log(err))
+  getData('shields');
 }
 
 const getWeapons = () => {
-  const URL = 'https://eldenring.fanapis.com/api/weapons?limit=100';
-  receivedData = fetch(URL)
-    .then(res => res.json())
-    .then(data => receivedData = data.data)
-    .then(() => console.log(receivedData))
-    .then(() => receivedData.forEach(data => createImageList(data)))
-    .then(err => console.log(err))
+  getData('weapons');
 }
 
 const createImageList = (data) => {
   let img = document.createElement('img');
   img.src = data.image;
   document.querySelector('.card-list').appendChild(img);
+}
+
+const getData = (dataType) => {
+  const URL = `https://eldenring.fanapis.com/api/${dataType}?limit=100`;
+
+  receivedData = fetch(URL)
+    .then(res => res.json())
+    .then(data => receivedData = data.data)
+    .then(() => console.log(receivedData))
+    .then(() => receivedData.forEach(data => createImageList(data)))
+    .then(err => console.log(err))
 }
 
 shieldsButton.addEventListener('click', getShields);
